adc66fe44248962d369124f1e2b284cb03199d19
im-hamza-dev <hi.hamza.dev@gmail.com>
2025-03-07 21:37:49 +0500

    removed un used import in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import { MdSearch, MdAccountCircle } from 'react-icons/md';
-import { IoNotificationsOutline } from 'react-icons/io5';
 import Link from 'next/link';
 import Navbar from "./components/Navbar/navbar";
 import Banner from "./components/banner";
